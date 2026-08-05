b978669f776f92097bf787456d8730ab1ede255f
ooesophagus <2504440E@student.tp.edu.sg>
2026-08-06 01:13:17 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,73 +2,46 @@ const API =
 "https://bxfxjwer34.execute-api.ap-southeast-1.amazonaws.com";
 
 
+async function adminFetch(url, options = {}) {
 
-async function adminFetch(url,options={}){
-
-
-const response =
-await fetch(
-url,
-{
-
-...options,
-
-headers:{
-
-"Content-Type":"application/json"
-
-}
-
-}
-);
+    const response = await fetch(
+        url,
+        {
+            ...options,
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }
+    );
 
 
+    if(!response.ok){
+        throw new Error("Request failed");
+    }
 
-if(!response.ok){
 
-throw new Error(
-"Request failed"
-);
+    return response.json();
 
 }
-
-
-
-return response.json();
-
-
-
-}
-
 
 
 
 function showAdminMessage(
-id,
-message,
-isError=false
+    id,
+    message,
+    isError=false
 ){
 
-
-const el=document.getElementById(id);
-
-
-if(el){
-
-el.textContent=message;
-
-el.style.color =
-isError ? "red":"green";
+    const el = document.getElementById(id);
 
 
-}
+    if(el){
 
+        el.textContent = message;
 
-}
+        el.style.color =
+        isError ? "red" : "green";
 
-
-    element.textContent=message;
-
-    element.style.color=isError?"#d62828":"#0f6eff";
+    }
 
 }
